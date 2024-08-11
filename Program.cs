00d42878aad634f17276a8e47431eac2d221509f
KavishkaSasindu <sasindu0215@gmail.com>
2024-08-11 22:29:25 +0530

    initialize commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyApp
+{
+     class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter your name :");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age :");
+            int age = Convert.ToInt32(Console.ReadLine()); 
+
+            if(age>=18)
+            {
+                Console.WriteLine("You can access the adminboard");
+            }
+            else{
+                Console.WriteLine("No you can not access the admin board");
+            }
+        }
+    }
+}
