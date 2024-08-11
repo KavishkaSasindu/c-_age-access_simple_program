0948154837fc87929b04ec0e68c96745ef511a06
KavishkaSasindu <sasindu0215@gmail.com>
2024-08-11 23:22:54 +0530

    updated by ksdev

--- Program.cs
+++ Program.cs
@@ -6,35 +6,44 @@ namespace MyApp
     {
         public static void Main(string[] args)
         {
-            Console.Write("Enter your name :");
-            string name = Console.ReadLine();
+            // Console.Write("Enter your name :");
+            // string name = Console.ReadLine();
 
-            Console.Write("Enter your age :");
-            int age = Convert.ToInt32(Console.ReadLine()); 
+            // Console.Write("Enter your age :");
+            // int age = Convert.ToInt32(Console.ReadLine()); 
 
-            if(age>=18)
-            {
-                Console.WriteLine("You can access the adminboard");
-            }
-            else{
-                Console.WriteLine("No you can not access the admin board");
-            }
+            // if(age>=18)
+            // {
+            //     Console.WriteLine("You can access the adminboard");
+            // }
+            // else{
+            //     Console.WriteLine("No you can not access the admin board");
+            // }
 
-            Console.Write("Enter your day number :");
-            int dayNumber = Convert.ToInt32(Console.ReadLine());    
+            // Console.Write("Enter your day number :");
+            // int dayNumber = Convert.ToInt32(Console.ReadLine());    
 
-            switch(dayNumber)
-            {
-                case 1:
-                    Console.WriteLine("Monday");
-                    break;
-                case 2:
-                    Console.WriteLine("Tuesday");
-                    break ;
-                default:
-                    Console.WriteLine("Enter valid number");
-                    break;
-            }
+            // switch(dayNumber)
+            // {
+            //     case 1:
+            //         Console.WriteLine("Monday");
+            //         break;
+            //     case 2:
+            //         Console.WriteLine("Tuesday");
+            //         break ;
+            //     default:
+            //         Console.WriteLine("Enter valid number");
+            //         break;
+            // }
+
+                Console.WriteLine("Enter how many times do you need to say 'i love you'");
+
+                int howTime = Convert.ToInt32(Console.ReadLine());
+
+                for(int i =1;i<=howTime;i++)
+                {
+                    Console.WriteLine("I Love You "+i);
+                }
         }
     }
 }
