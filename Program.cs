94c796930e27a68a6d1c45088822e0481241a2ed
KavishkaSasindu <sasindu0215@gmail.com>
2024-08-11 23:06:28 +0530

    updated by ksdev

--- Program.cs
+++ Program.cs
@@ -19,6 +19,22 @@ namespace MyApp
             else{
                 Console.WriteLine("No you can not access the admin board");
             }
+
+            Console.Write("Enter your day number :");
+            int dayNumber = Convert.ToInt32(Console.ReadLine());    
+
+            switch(dayNumber)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break ;
+                default:
+                    Console.WriteLine("Enter valid number");
+                    break;
+            }
         }
     }
 }
